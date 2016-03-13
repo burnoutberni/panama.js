c5e10916677fff0727f82bb27a7fa3850472257e
phaer <phaer@phaer.org>
2016-03-13 06:22:13 +0100

    let youTubeDl return an promise.

--- src/playlist.js
+++ src/playlist.js
@@ -27,25 +27,42 @@ export default class Playlist {
             }
             return element;
         });
- 
     }
 
     add(requestUrl) {
-        youTubeDl(requestUrl).then(
-            result => {
-                const [stdout, stderr] = result,
-                      title = stdout[0].trim(),
-                      url = stdout[1].trim();
-                console.log('youtube-dl stderr', stderr);
-                this.mpv.command('show-text', `added ${title} (${url})`);
-                this.mpv.command('get_property', 'playlist/count').then(
-                    count => {
-                        this.items[count] = {title, requestUrl};
-                        this.mpv.command('loadfile', url, 'append', `force-media-title="${title}"`)
-                            .then(v => console.log('loadfile', title, url, v),
-                                  e => console.log('loadfile', e));
-                    },
-                    stderr => this.mpv.command('show-text', stderr.join('\n')));
-            });
+        return new Promise((resolve, reject) => {
+            youTubeDl(requestUrl).then(
+                result => {
+                    const [stdout, stderr] = result,
+                          title = stdout[0].trim(),
+                          url = stdout[1].trim();
+
+                    console.log('[youtube-dl] out: ', stdout);
+                    console.log('[youtube-dl] err: ', stderr);
+
+                    if (stderr.length) {
+                        this.mpv.command('show-text', stderr.join('\n'));
+                    }
+
+                    if (title === '' || url === '') {
+                        reject('[youtube-dl] returned nothing.');
+                        return;
+                    }
+
+                    this.mpv.command('show-text', `added ${title} (${url}) to playlist.`);
+
+                    this.items[requestUrl] = {title};
+                    this.save();
+
+                    console.info('[youtube-dl] loadfile: ', url);
+                    this.mpv.command('loadfile', url, 'append-play', `title="${title}"`)
+                        .then(v => resolve(v),
+                              e => reject(e));
+                },
+                error => reject(`[youtube-dl] error: ${error}`)
+            );
+        });
+    }
+
     }
 }
