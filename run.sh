255decfbe2702e978835a65bfe9e3352b97a8d4d
burnoutberni <hello@nini.su>
2017-03-24 11:40:22 +0100

    make sure there's nothing blocking the mpv.socket, no longer default to pause

--- run.sh
+++ run.sh
@@ -3,9 +3,10 @@
 set -xe
 trap "kill -SIGTERM 0" SIGINT EXIT
 
+rm -f /tmp/mpv.socket && \
 mpv --input-unix-socket /tmp/mpv.socket \
     --ytdl-format=best \
     --reset-on-next-file=all \
-    --idle=yes --keep-open=yes --pause &
+    --idle=yes --keep-open=yes &
 
 node dist/server.js
